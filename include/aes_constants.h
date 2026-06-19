ddb0a39a0d4c4909a5965991643638b8ef275cbb
Dmitry Kudryashov <dimakudrashov13@gmail.com>
2026-06-19 20:50:02 +0300

    Update constants defines

--- include/aes_constants.h
+++ include/aes_constants.h
@@ -3,5 +3,11 @@
 #pragma once
 
 #define Nb 4 /* Number of columns (32-bit words) comprising the State. For AES, this is always 4. */
+
+#ifndef Nk
 #define Nk 4 /* Number of 32-bit words comprising the Cipher Key. For AES-128, this is 4, for AES-192, this is 6, and for AES-256, this is 8. */
+#endif
+
+#ifndef Nr
 #define Nr 10 /* Number of rounds. For AES-128, this is 10, for AES-192, this is 12, and for AES-256, this is 14. */
+#endif
