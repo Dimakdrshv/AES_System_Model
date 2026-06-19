bf7dc0d6149fd3b5eb77720a703aaefd46c0e9ff
Dmitry Kudryashov <dimakudrashov13@gmail.com>
2026-06-19 20:03:05 +0300

    Add additional decryption method

--- include/aes.h
+++ include/aes.h
@@ -5,4 +5,5 @@
 #include <stdint.h>
 
 void Cipher(uint8_t* in, uint8_t* out, uint32_t* w);
-void EqInvCipher(uint8_t* in, uint8_t* out, uint32_t* w); 
+void EqInvCipher(uint8_t* in, uint8_t* out, uint32_t* w);
+void InvCipher(uint8_t* in, uint8_t* out, uint32_t* w);
--- src/aes.c
+++ src/aes.c
@@ -30,6 +30,33 @@ void Cipher(uint8_t in[4*Nb], uint8_t out[4*Nb], uint32_t w[Nb * (Nr + 1)]) {
     out[3] = state[3][0]; out[7] = state[3][1]; out[11] = state[3][2]; out[15] = state[3][3];
 }
 
+void InvCipher(uint8_t in[4*Nb], uint8_t out[4*Nb], uint32_t w[Nb * (Nr + 1)]) {
+    uint8_t state[4][4]; // always a 4x4 matrix
+
+    state[0][0] = in[0]; state[0][1] = in[4]; state[0][2] = in[8]; state[0][3] = in[12];
+    state[1][0] = in[1]; state[1][1] = in[5]; state[1][2] = in[9]; state[1][3] = in[13];
+    state[2][0] = in[2]; state[2][1] = in[6]; state[2][2] = in[10]; state[2][3] = in[14];
+    state[3][0] = in[3]; state[3][1] = in[7]; state[3][2] = in[11]; state[3][3] = in[15];
+
+    AddRoundKey(state, w, Nr);
+
+    for (size_t round = Nr - 1; round > 0; round--) {
+        InvShiftRows(state);
+        InvSubBytes(state);
+        InvAddRoundKey(state, w, round);
+        InvMixColumns(state);
+    }
+
+    InvShiftRows(state);
+    InvSubBytes(state);
+    InvAddRoundKey(state, w, 0);
+
+    out[0] = state[0][0]; out[4] = state[0][1]; out[8] = state[0][2]; out[12] = state[0][3];
+    out[1] = state[1][0]; out[5] = state[1][1]; out[9] = state[1][2]; out[13] = state[1][3];
+    out[2] = state[2][0]; out[6] = state[2][1]; out[10] = state[2][2]; out[14] = state[2][3];
+    out[3] = state[3][0]; out[7] = state[3][1]; out[11] = state[3][2]; out[15] = state[3][3];
+}
+
 void EqInvCipher(uint8_t in[4*Nb], uint8_t out[4*Nb], uint32_t w[Nb * (Nr + 1)]) {
     /* dw[0:3] = w[0:3], dw[4:39] = InvMixColumns(w[4:39]), dw[40:43] = w[40:43]*/
     uint32_t dw[Nb * (Nr + 1)];
